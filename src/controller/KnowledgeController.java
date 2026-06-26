fd396134cc5152bfa5db3fb5606d11c81ae9835f
Stiven Cahyono <stiv2438@gmail.com>
2026-06-26 17:34:51 +0700

    docs: improve model layer documentation

--- src/controller/KnowledgeController.java
+++ src/controller/KnowledgeController.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.io.FileWriter;
 
 /**
- * Class KnowledgeController berfungsi sebagai penghubung antara View dan Model.
+ * Class KnowledgeController berfungsi sebagai penghubung antara view dan model.
  * Controller mengatur proses tambah data, pencarian, filter, statistik,
  * penghapusan data, sorting, dan ekspor laporan.
  */
