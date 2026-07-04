3350f2f1b3b004a9e375fee7fb02879738d09805
Stiven Cahyono <stiv2438@gmail.com>
2026-07-04 21:01:33 +0700

    refactor(model): improve OOP implementation and documentation

--- src/model/EntitasHukum.java
+++ src/model/EntitasHukum.java
@@ -1,7 +1,44 @@
 package model;
 
+/**
+ * Class induk yang menyimpan data umum entitas hukum.
+ * Saat ini class ini menyimpan nomor perkara yang akan
+ * diwariskan ke class Putusan.
+ */
 public class EntitasHukum {
 
-    protected String nomorPerkara;
+    private String nomorPerkara;
 
+    /**
+     * Constructor kosong.
+     */
+    public EntitasHukum() {
+    }
+
+    /**
+     * Constructor dengan parameter nomor perkara.
+     *
+     * @param nomorPerkara nomor perkara putusan
+     */
+    public EntitasHukum(String nomorPerkara) {
+        this.nomorPerkara = nomorPerkara;
+    }
+
+    /**
+     * Mengambil nomor perkara.
+     *
+     * @return nomor perkara
+     */
+    public String getNomorPerkara() {
+        return nomorPerkara;
+    }
+
+    /**
+     * Mengubah nomor perkara.
+     *
+     * @param nomorPerkara nomor perkara baru
+     */
+    public void setNomorPerkara(String nomorPerkara) {
+        this.nomorPerkara = nomorPerkara;
+    }
 }
--- src/model/Printable.java
+++ src/model/Printable.java
@@ -1,7 +1,15 @@
 package model;
 
+/**
+ * Interface untuk objek yang dapat menampilkan informasi.
+ * Class yang mengimplementasikan interface ini wajib
+ * menyediakan method printInfo().
+ */
 public interface Printable {
 
+    /**
+     * Menampilkan informasi objek.
+     */
     void printInfo();
 
 }
